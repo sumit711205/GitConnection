39b05904c9fe181991b90bff1d2ff065f8592f88
Your Name <you@example.com>
2022-02-22 23:09:55 +0530

    Lead info added

--- GitConnectionV1/src/Test1.java
+++ GitConnectionV1/src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		String hungry="yes";
 		//token : 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
-		// sumit711205 act like team lead and developer-Z
+		// sumit711205 act like team lead who will approve the pull request and developer-Z
 		if(hungry=="yes")
 		{
 			System.out.println("Start eating");
